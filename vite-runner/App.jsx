81703c1ed72da26547f9c0d2187fe1326579bf79
Aybuai <13624448268@163.com>
2024-03-24 22:25:03 +0800

    feat: 使用vite支持 jsx文件

---
+++ vite-runner/App.jsx
@@ -0,0 +1,13 @@
+import React from "./core/React.js";
+
+// const App = React.createElement("div", { id: "app" }, "hello ", "mini-react!3");
+
+const App = <div id="app">hello mini-react <div id="haha">haha</div></div>
+// console.log(App, 'App')
+
+// function AppOne() {
+//   return <div id="app">hello mini-react</div>
+// }
+// console.log(AppOne, 'AppOne')
+
+export default App;
